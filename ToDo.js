35110a3de1f2d5f1df0fc87fec92f46af054dcfa
BackYoungJae <byj010101@gmail.com>
2018-07-25 17:16:44 +0900

    complete

---
+++ ToDo.js
@@ -0,0 +1,167 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions, TextInput } from 'react-native';
+import PropTypes from 'prop-types';
+
+const {width, height } = Dimensions.get('window');
+
+export default class ToDo extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isEditing: false,
+      isCompleted: false,
+      toDoValue: props.text
+    }
+  }
+
+  static propTypes = {
+    text : PropTypes.string.isRequired,
+    isCompleted : PropTypes.bool.isRequired,
+    deleteToDo: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired,
+    uncompleteToDo : PropTypes.func.isRequired,
+    completeToDo: PropTypes.func.isRequired,
+    updateToDo: PropTypes.func.isRequired
+  }
+
+
+  render(){
+    const {isEditing, toDoValue } = this.state;
+    const {text, id, deleteToDo, isCompleted} = this.props;
+    return(
+      <View style = {styles.container}>
+        <View style = {styles.column}>
+          <TouchableOpacity onPress = {this._toggleComplete}>
+            <View style = {[styles.circle, isCompleted ? styles.completed : styles.uncompleted]}/>
+          </TouchableOpacity>
+          {isEditing ? (
+            <TextInput 
+              style={[styles.text, styles.input, isCompleted ? styles.completedText : styles.uncompletedText]} 
+              value={toDoValue} 
+              multiline={true}
+              onChangeText = {this._controlInput}
+              returnKeyType = {'done'}
+              onBlur = {this._endEditing}
+              underlineColorAndroid = {"transparent"}
+            />
+            ) : (
+              <Text 
+                style={[styles.text, isCompleted ? styles.completedText : styles.uncompletedText]}
+              >
+                {text}
+              </Text>)}
+        </View>
+
+          {isEditing ? (
+            <View style={styles.actions}>
+              <TouchableOpacity onPress={this._endEditing}>
+                <View style={styles.actionContainer}>
+                  <Text style={styles.actionText}>V</Text>
+                </View>
+              </TouchableOpacity>
+            </View>)
+             : 
+            (<View style={styles.actions}>
+              <TouchableOpacity onPressOut={this._startEditing}>
+                <View style={styles.actionContainer}>
+                  <Text style={styles.actionText}>수정</Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPressOut = {(e) => {e.stopPropagation(); deleteToDo(id)}}>
+                <View style={styles.actionContainer}>
+                  <Text style={styles.actionText}>삭제</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          )}
+      </View>
+    );
+  }
+  _toggleComplete = (e) => {
+    e.stopPropagation()
+    const { isCompleted, uncompleteToDo, completeToDo, id} = this.props;
+    if(isCompleted){
+      uncompleteToDo(id);
+    } else {
+      completeToDo(id);
+    }
+  };
+
+  _startEditing = (e) => {
+    e.stopPropagation()
+    this.setState({
+      isEditing: true
+    })
+  };
+
+  _endEditing = (e) => {
+    e.stopPropagation()
+    const { toDoValue } = this.state;
+    const { id, updateToDo } = this.props;
+    updateToDo(id, toDoValue);
+    this.setState({
+      isEditing: false
+    })
+  }
+
+  _controlInput = text => {
+    this.setState({
+      toDoValue : text
+    })
+  }
+}
+
+const styles = StyleSheet.create({
+  container : {
+    width: width - 50,
+    borderBottomColor: '#bbb',
+    borderBottomWidth : StyleSheet.hairlineWidth,
+    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "space-between"
+  }, 
+  text : {
+    fontWeight: "600",
+    fontSize: 20,
+    marginVertical:20
+  },
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderColor: "red",
+    borderWidth: 3,
+    marginRight: 20
+  },
+  completed: {
+    borderColor : "#bbb"
+  },
+  uncompleted: {
+    borderColor : "#f23657"
+  }, 
+  completedText: {
+    color: "#bbb",
+    textDecorationLine: "line-through"
+  },
+  uncompletedText: {
+    color: "#353839"
+  },
+  column: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: width/2
+  },
+  actions: {
+    flexDirection: "row",
+  },
+  actionContainer: {
+    marginVertical:10,
+    marginHorizontal: 10
+  },
+  input: {
+    width: width/2,
+    marginVertical: 15,
+    paddingBottom: 5
+  }
+
+})
